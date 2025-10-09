557a7a149bb720822907ce8e9b0e8322317c6f67
rezairanshahi021 <147942853+rezairanshahi021@users.noreply.github.com>
2025-10-09 10:25:07 +0330

    configures mongo db

---
+++ zoochallenge/src/main/java/org/iranshahi/zoochallenge/configs/MongoConfig.java
@@ -0,0 +1,9 @@
+package org.iranshahi.zoochallenge.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
