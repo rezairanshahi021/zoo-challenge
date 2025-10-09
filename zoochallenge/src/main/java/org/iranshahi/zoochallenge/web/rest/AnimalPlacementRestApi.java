0052a1a6f66939916c057ce816b43bf77674d6ea
rezairanshahi021 <147942853+rezairanshahi021@users.noreply.github.com>
2025-10-09 20:52:37 +0330

    improves placement exposed api according to RestFul standard

--- zoochallenge/src/main/java/org/iranshahi/zoochallenge/web/rest/AnimalPlacementRestApi.java
+++ zoochallenge/src/main/java/org/iranshahi/zoochallenge/web/rest/AnimalPlacementRestApi.java
@@ -2,23 +2,24 @@ package org.iranshahi.zoochallenge.web.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.iranshahi.zoochallenge.business.dto.AnimalDto;
+import org.iranshahi.zoochallenge.business.dto.AnimalPlacementDto;
 import org.iranshahi.zoochallenge.business.service.AnimalPlacementService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/animals/placement")
+@RequestMapping("/api/animals/{animal-id}/placement")
 @RequiredArgsConstructor
 public class AnimalPlacementRestApi {
     private final AnimalPlacementService animalPlacementService;
 
-    @PatchMapping("/{animal-id}/room/{room-id}")
-    public ResponseEntity<AnimalDto> place(@PathVariable("animal-id") String id, @RequestParam("room-id") String roomId) {
-        return ResponseEntity.ok(animalPlacementService.place(id, roomId));
+    @PatchMapping
+    public ResponseEntity<AnimalDto> place(@PathVariable("animal-id") String id, @RequestBody AnimalPlacementDto placementDto) {
+        return ResponseEntity.ok(animalPlacementService.place(id, placementDto.roomId()));
     }
 
 
-    @DeleteMapping("/{animal-id}/room")
+    @DeleteMapping
     public ResponseEntity<AnimalDto> removeFromRoom(@PathVariable("animal-id") String id) {
         return ResponseEntity.ok(animalPlacementService.remove(id));
     }
