77d1d0d4f48fdaa7e66c6ae3a32c661eeacac7e7
rezairanshahi021 <147942853+rezairanshahi021@users.noreply.github.com>
2025-10-09 15:22:55 +0330

    configures logback and aspect to log request and response

---
+++ zoochallenge/src/main/java/org/iranshahi/zoochallenge/configs/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package org.iranshahi.zoochallenge.configs;
+
+
+import org.iranshahi.zoochallenge.aspect.log.LoggingAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+}
